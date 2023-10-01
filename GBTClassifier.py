f99889f12e7dfc7fbb8e836651b8016cebd16700
Natalia Khaidanova <khaidanova@outlook.com>
2023-10-01 14:50:07 +0200

    Update GBTClassifier.py

--- GBTClassifier.py
+++ GBTClassifier.py
@@ -1,5 +1,6 @@
 from pyspark.ml.classification import GBTClassifier
 from pyspark.ml.evaluation import BinaryClassificationEvaluator
+from utils import *
 
 flights_train, flights_test = train_test_split('classification')
 
@@ -14,38 +15,40 @@ def GBT_classifier(flights_train, flights_test):
     :param list flights_test: list of test data with features 
     :return: none
     """
-    gbt = GBTClassifier()
-    gbt.fit(flights_train)
-    
+    gbt = GBTClassifier().fit(flights_train)
+
     # Get AUC on test data
     evaluator = BinaryClassificationEvaluator()
     predictions = gbt.transform(flights_test)
     print('AUC of the default GBTClassifier = {}\n' .format(evaluator.evaluate(predictions)))
 
     # Find the number of trees and the relative importance of features
-    # print(gbt.trees)
+    #print(gbt.trees)
     print(gbt.featureImportances, '\n')
+
+    # RESULT:    
+    # AUC of the default GBTClassifier = 0.7296886919893384
 
     # Perfom hyperparameter tuning 
     params = ParamGridBuilder()
 
-    params = params.addGrid(gbt.maxDepth, [3, 5, 7]) \
+    params = params.addGrid(gbt.maxDepth, [5, 7]) \
                    .addGrid(gbt.maxBins, [32, 64, 128]) \
-                   .addGrid(gbt.maxIter, [10, 20, 50]) \
-                   .addGrid(gbt.stepSize, [0.01, 0.1, 0.2]) \
+                   .addGrid(gbt.maxIter, [20, 50]) \
+                   .addGrid(gbt.stepSize, [0.01, 0.1]) \
                    .addGrid(gbt.impurity, ['entropy', 'gini', 'variance']) \
                    .build() 
 
     # Other hyperparameters to consider: 
-    # minInstancesPerNode, [1, 2, 5]
+    # minInstancesPerNode, [1, 2, 3]
     # minInfoGain, [0.0, 0.01, 0.1]) 
-    # subsamplingRate, [0.7, 0.8, 1.0]
+    # subsamplingRate, [0.8, 0.9, 1.0]
     # featureSubsetStrategy, ['all', 'onethird', 'sqrt']
     # validationTol, [0.001, 0.01, 0.1]
 
     # Perform cross-validation 
     gbt = GBTClassifier() 
-    cv = CrossValidator(estimator=gbt, estimatorParamMaps=params, evaluator=evaluator, numFolds=5)
+    cv = CrossValidator(estimator=gbt, estimatorParamMaps=params, evaluator=evaluator, numFolds=3)
 
     cv = cv.fit(flights_train)
 
@@ -60,19 +63,16 @@ def GBT_classifier(flights_train, flights_test):
     print('Best Param (impurity):', best_model.getImpurity(), best_model.explainParam('impurity'))
 
     # RESULT: 
-    # Best Param (maxIter): 650 (default: 100, current: 650)
-    # Best Param (regParam): 0.0 (default: 0.0, current: 0.0)
-    # Best Param (elasticNetParam): 0.0 (default: 0.0, current: 0.0)
-    # Best Param (fitIntercept): False (default: True, current: False)
+    # Best Param (maxDepth): 5 (default: 5)
+    # Best Param (maxBins): 32 (default: 32)
+    # Best Param (maxIter): 20 (default: 20)
+    # Best Param (stepSize): 0.1 (default: 0.1)
+    # Best Param (impurity): variance impurity (default: variance)
 
     predictions = best_model.transform(flights_test)
-    print('AUC of the best GBTClassifier = {}\n' .format(evaluator.evaluate(predictions)))
-    # print(gbt.trees)
-    print(gbt.featureImportances)
-
-    zero_coeff = len([beta for beta in best_model.coefficients if beta == 0])
-    print('Number of coefficients equal to 0: {}\n' .format(int(zero_coeff)))
+    print('\nAUC of the best GBTClassifier = {}\n' .format(evaluator.evaluate(predictions)))
+    #print(gbt.trees)
+    print(best_model.featureImportances)
 
     # RESULT:
-    # RMSE of the best model = 7.671071994215025e-14
-    # Number of coefficients equal to 0: 0
+    # AUC of the best GBTClassifier = 0.7267226346255101
