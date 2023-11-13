c157f0d5bfbdf34b96523a4780e399eb44f9ea6a
Natalia Khaidanova <khaidanova@outlook.com>
2023-11-13 18:10:21 +0100

    Finish utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import round, when
 from pyspark.ml.feature import StringIndexer, OneHotEncoder, VectorAssembler, Bucketizer
-
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator, BinaryClassificationEvaluator
 
 # Create SparkSession object
 spark = SparkSession.builder \
@@ -87,3 +87,37 @@ def train_test_split(task):
     #print('Training ratio to the whole data: {}.' .format(training_ratio))
     
     return flights_train, flights_test
+
+
+# ADDITIONAL 
+def classification_evaluation(prediction):
+    """
+    Test a classification model with accuracy, precision, recall, f1 score, and AUC.
+    
+    :param list prediction: list with model's predictions  
+    :return: none
+    """
+    multi_evaluator = MulticlassClassificationEvaluator()
+    binary_evaluator = BinaryClassificationEvaluator()
+
+    # Calculate the elements of the confusion matrix
+    TN = prediction.filter('prediction = 0 AND label = prediction').count()
+    TP = prediction.filter('prediction = 1 AND label = prediction').count()
+    FN = prediction.filter('prediction = 0 AND label != prediction').count()
+    FP = prediction.filter('prediction = 1 AND label != prediction').count()
+
+    accuracy = multi_evaluator.evaluate(prediction, {multi_evaluator.metricName: 'accuracy'})
+    # OR
+    #accuracy = (TP + TN) / len(prediction)
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    f1_score = 2 * (precision * recall) / (precision + recall)
+
+    auc = binary_evaluator.evaluate(prediction, {binary_evaluator.metricName: 'areaUnderROC'})
+
+    print('''
+    accuracy  = {:.2f}
+    precision = {:.2f}
+    recall    = {:.2f}
+    f1 score  = {:.2f}
+    AUC       = {:.2f}'''.format(accuracy, precision, recall, f1_score, auc))
