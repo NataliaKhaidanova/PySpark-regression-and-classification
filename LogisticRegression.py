80d2702702d294b4f12ecaea989672ef432a2ffe
Natalia Khaidanova <khaidanova@outlook.com>
2023-11-13 18:26:38 +0100

    Update LogisticRegression.py

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,33 +1,69 @@
+# Classification - AUC
+# Logistic Regression with cross-validation and hyperparameter tuning 
 from pyspark.ml.classification import LogisticRegression
-from pyspark.ml.evaluation import MulticlassClassificationEvaluator, BinaryClassificationEvaluator
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+from pyspark.ml.tuning import ParamGridBuilder
+from pyspark.ml.tuning import CrossValidator
 from utils import *
+
 
 flights_train, flights_test = train_test_split('classification')
 
-# Create a classifier object and train on training data
-logistic = LogisticRegression().fit(flights_train)
 
-# Create predictions for the testing data and show confusion matrix
-prediction = logistic.transform(flights_test)
-#prediction.groupBy('label', 'prediction').count().show()
+def logistic_regression(flights_train, flights_test):
+    """
+    Train and test (using AUC) LogisticRegression model,
+    Perform hyperparameter tuning and cross-validation, 
+    Evaluate the best model,
+    Compare the default model to the best model. 
+    
+    :param list flights_train: list of training data with features 
+    :param list flights_test: list of test data with features 
+    :return: none
+    """
+    logistic = LogisticRegression().fit(flights_train)
 
-# Calculate the elements of the confusion matrix
-TN = prediction.filter('prediction = 0 AND label = prediction').count()
-TP = prediction.filter('prediction = 1 AND label = prediction').count()
-FN = prediction.filter('prediction = 0 AND label != prediction').count()
-FP = prediction.filter('prediction = 1 AND label != prediction').count()
+    # Get AUC on test data
+    evaluator = BinaryClassificationEvaluator()
+    predictions = logistic.transform(flights_test)
+    print('AUC of the default LogisticRegression = {}\n' .format(evaluator.evaluate(predictions)))
 
-multi_evaluator = MulticlassClassificationEvaluator()
-binary_evaluator = BinaryClassificationEvaluator()
+    # RESULT:    
+    # AUC of the default LogisticRegression = 0.6492130116838126
+                                          
+    # Perfom hyperparameter tuning 
+    params = ParamGridBuilder()
 
-accuracy = multi_evaluator.evaluate(prediction, {multi_evaluator.metricName: "accuracy"})
-precision = TP / (TP + FP)
-recall = TP / (TP + FN)
-f1_score = 2 * (precision * recall) / (precision + recall)
-auc = binary_evaluator.evaluate(prediction, {binary_evaluator.metricName: "areaUnderROC"})
+    params = params.addGrid(logistic.maxIter, [10, 50, 100]) \
+                   .addGrid(logistic.regParam, [0.01, 0.1, 1.0]) \
+                   .addGrid(logistic.elasticNetParam, [0.0, 0.5, 1.0]) \
+                   .build()
 
-print('''accuracy  = {:.2f}
-precision = {:.2f}
-recall    = {:.2f}
-f1 score  = {:.2f}
-AUC       = {:.2f}'''.format(accuracy, precision, recall, f1_score, auc))
+    # Perform cross-validation 
+    logistic = LogisticRegression() 
+    cv = CrossValidator(estimator=logistic, estimatorParamMaps=params, evaluator=evaluator, numFolds=5)
+
+    cv = cv.fit(flights_train)
+
+    # Extract the best model 
+    best_model = cv.bestModel
+
+    # Look at the hyperparameters in the best model
+    print('Best Param (maxIter):', best_model.getMaxIter(), best_model.explainParam('maxIter'))
+    print('Best Param (regParam):', best_model.getRegParam(), best_model.explainParam('regParam'))
+    print('Best Param (elasticNetParam):', best_model.getElasticNetParam(), best_model.explainParam('elasticNetParam'))
+
+    # RESULT: 
+    # Best Param (maxIter): 100 (default: 100)
+    # Best Param (regParam): 0.0 (default: 0.0)
+    # Best Param (elasticNetParam): 0.0 (default: 0.0)
+
+    predictions = best_model.transform(flights_test)
+    print('\nAUC of the best LogisticRegression = {}\n' .format(evaluator.evaluate(predictions)))
+
+    # RESULT:
+    # AUC of the best LogisticRegression = 0.6492098424114412
+
+
+if __name__ == '__main__':
+    logistic_regression(flights_train, flights_test)
